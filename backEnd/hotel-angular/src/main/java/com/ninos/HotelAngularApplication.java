cacd1591f77f80f9b0fc416c21ad0c00ff871640
NinoosMoshi <nalkaton@gmail.com>
2025-02-23 18:04:42 -0600

    init a project

---
+++ backEnd/hotel-angular/src/main/java/com/ninos/HotelAngularApplication.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAngularApplication.class, args);
+	}
+
+}
